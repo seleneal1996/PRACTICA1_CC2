7ec4c2233a8752130c404efaa2c30410cdc93987
seleneal1996 <selenemsb@hotmail.com>
2018-04-05 17:53:13 -0500

    ejercicio 1

--- Ejercicio1/main.cpp
+++ Ejercicio1/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
 using namespace std;
+//Implementar una funcíon donde se muestre el paso por valor y referencia.
 
+//PASO POR VALOR : se copia
+int suma1(int n)
+{
+    return n+1;
+}
+//PASO POR REFERENCIA: se refiere
+void incrementa1(int &n)
+{
+    n=n+1;
+}
 int main()
 {
-    cout << char(9+48) << "o"<<endl;
-    return 0;
+ int a=1,b=2,c;
+ c=suma1(a);
+ incrementa1(b);
+ cout<<c<<" "<<b<<endl;
 }
