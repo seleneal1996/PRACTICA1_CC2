2ab47140c760a4a217d37746e94109f97a99c028
seleneal1996 <selenemsb@hotmail.com>
2018-04-05 20:00:57 -0500

    ejercicio 2

--- Ejercicio2/main.cpp
+++ Ejercicio2/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+int suma(int a[],int tam)
+{
+    for(int k=0;k<tam;k++)
+    {
+        return a[tam-1]+suma(a,tam-1);
+    }
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int tam,a[100];
+    cout<<"¿Tamaño del arreglo?"<<endl;
+    cin>>tam;
+    for(int i=0;i<tam;i++)
+    {
+        cout<<"a["<<i+1<<"]=";
+        cin>>a[i];
+    }
+    cout<<"SUMA=>"<<suma(a,tam);
 }
+
