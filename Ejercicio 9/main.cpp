2cce55db243e737335ef0255e49ee5986786def3
seleneal1996 <selenemsb@hotmail.com>
2018-04-07 23:29:52 -0500

    ejercicio 9

--- Ejercicio 9/main.cpp
+++ Ejercicio 9/main.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
-#include<string.h>
-#include<stdbool.h>
 using namespace std;
-bool esPalindroma(char cadena[],char copia[]);
+
+int longitud(char cadena[])
+{
+    int i=0;
+    //RECORRIDO
+    while(cadena[i]!='\0')
+    {
+        i++;
+    }
+    return i;
+}
+bool es_Palindrome(char cadena[],char copia[])
+{
+    int tamanio=longitud(cadena);
+    for(int i=(tamanio-1);i>=0;i--)
+    {
+        //NOS ENCONTRAMOS EN LA PRIMERA POSICION DE LA COPIA EN LA CUAL TENEMOS LA ULTIMA LETRA DE LA CADENA
+        copia[tamanio-1-i]=cadena[i];
+    }
+    //VALIDAR LA CADENA
+    copia[longitud(cadena)]='\0';
+    //COMPARACION DE CADENAS
+
+    for(int i=0;i<tamanio;i++)
+    {
+        if(cadena[i]!=copia[i])
+            return false;
+        return true;
+    }
+}
 int main()
 {
-    bool retorno=0;
+    bool retorno;
     char cadena[100],copia[100];
     cout << "Ingrese palabra:" << endl;
     cin>>cadena;
-    esPalindroma(cadena,copia);
-    retorno=esPalindroma(cadena,copia);
-    if(retorno=esPalindroma(cadena,copia)==true)
+    es_Palindrome(cadena,copia);
+    retorno=es_Palindrome(cadena,copia);
+    if(retorno==true)
     {
         cout<<"SI ES PALINDROMA."<<endl;
     }
@@ -19,19 +46,4 @@ int main()
     {
         cout<<"NO ES PALINDROMA."<<endl;
     }
-}
-bool esPalindroma(char cadena[100],char copia[100])
-{
-
-     for(int i=(strlen(cadena)-1);i>=0;i--)
-    {
-        copia[strlen(cadena)-1-i]=cadena[i]; //NOS ENCONTRAMOS EN LA PRIMERA POSICION DE LA COPIA EN LA CUAL TENEMOS LA ULTIMA LETRA DE LA CADENA
-    }
-    copia[strlen(cadena)]='\0'; //VALIDAR LA CADENA
-    //COMPARACION DE CADENAS
-    if(strcmp(cadena,copia)==0)
-    {
-        return true;
-    }
-    return false;
 }
